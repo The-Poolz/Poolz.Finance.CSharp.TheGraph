2264cb0e7c7981a32443d4d1004451fec015ed49
ArdenHide <ardenhide@gmail.com>
2025-06-30 12:33:06 +0300

    Init project

---
+++ src/Poolz.Finance.CSharp.TheGraph.Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿using GraphQlClientGenerator;
+using EnvironmentManager.Extensions;
+using Poolz.Finance.CSharp.TheGraph.Generator;
+
+var url = Env.GRAPHQL_THE_GRAPH_URL.GetRequired();
+var schema = await GraphQlGenerator.RetrieveSchema(HttpMethod.Post, url);
+var configuration = new GraphQlGeneratorConfiguration
+{
+    TargetNamespace = Env.TARGET_NAMESPACE.GetRequired(),
+    IdTypeMapping = IdTypeMapping.String,
+};
+var generator = new GraphQlGenerator(configuration);
+var csharpCode = generator.GenerateFullClientCSharpFile(schema);
+await File.WriteAllTextAsync(Env.GENERATED_FILE_PATH.GetRequired(), csharpCode);
