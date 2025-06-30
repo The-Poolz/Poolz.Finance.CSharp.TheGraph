9db1e674d717b532e65eabfc88e70c69041d8e97
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-06-30 12:43:44 +0300

    Project init (#5)
    * Init project
    
    * - add and setting `ScalarFieldTypeMappingProvider`

---
+++ src/Poolz.Finance.CSharp.TheGraph.Generator/Env.cs
@@ -0,0 +1,8 @@
+﻿namespace Poolz.Finance.CSharp.TheGraph.Generator;
+
+public enum Env
+{
+    GRAPHQL_THE_GRAPH_URL,
+    TARGET_NAMESPACE,
+    GENERATED_FILE_PATH
+}
---
+++ src/Poolz.Finance.CSharp.TheGraph.Generator/Program.cs
@@ -0,0 +1,15 @@
+﻿using GraphQlClientGenerator;
+using EnvironmentManager.Extensions;
+using Poolz.Finance.CSharp.TheGraph.Generator;
+
+var url = Env.GRAPHQL_THE_GRAPH_URL.GetRequired();
+var schema = await GraphQlGenerator.RetrieveSchema(HttpMethod.Post, url);
+var configuration = new GraphQlGeneratorConfiguration
+{
+    TargetNamespace = Env.TARGET_NAMESPACE.GetRequired(),
+    IdTypeMapping = IdTypeMapping.String,
+    ScalarFieldTypeMappingProvider = new ScalarFieldTypeMappingProvider()
+};
+var generator = new GraphQlGenerator(configuration);
+var csharpCode = generator.GenerateFullClientCSharpFile(schema);
+await File.WriteAllTextAsync(Env.GENERATED_FILE_PATH.GetRequired(), csharpCode);
